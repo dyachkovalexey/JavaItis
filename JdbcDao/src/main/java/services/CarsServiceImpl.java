9209bdcaab820085ddbd5fd7ff71152e40c2c9f2
dyachkovalexey <dyachkov.95@bk.ru>
2016-10-13 16:51:55 +0300

    edit service

--- JdbcDao/src/main/java/services/CarsServiceImpl.java
+++ JdbcDao/src/main/java/services/CarsServiceImpl.java
@@ -1,7 +1,16 @@
 package services;
 
+import dao.CarsDao;
+import dao.CarsDaoJdbcImpl;
+
 /**
  * Created by KFU-user on 13.10.2016.
  */
 public class CarsServiceImpl implements CarsService {
+
+    private CarsDao carsDao;
+
+    public CarsServiceImpl(CarsDao carsDao) {
+        this.carsDao = carsDao;
+    }
 }
--- JdbcDao/src/main/java/services/OwnersServiceImpl.java
+++ JdbcDao/src/main/java/services/OwnersServiceImpl.java
@@ -6,4 +6,10 @@ import dao.OwnersDao;
  * Created by KFU-user on 13.10.2016.
  */
 public class OwnersServiceImpl implements OwnersService {
+
+    private OwnersDao ownersDao;
+
+    public OwnersServiceImpl(OwnersDao ownersDao) {
+        this.ownersDao = ownersDao;
+    }
 }
